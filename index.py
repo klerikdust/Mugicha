a590fdfe14a0499405eaf49627e74ea820b450a0
Naph <nightcore.raven07@gmail.com>
2019-08-10 22:16:52 +0700

    init

---
+++ index.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>', description='A bot that greets the user back.')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
